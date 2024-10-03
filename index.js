ab9f8e4c87d648ba85af977e76c64cfadb6954d2
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 15:10:58 -0700

    Updated by Phantomaton

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 import gallows from 'gallows';
-import * as projects from './projects.js';
+import createProjects from './projects.js';
 
 export default function phantomatonProjects(options = {}) {
+  const projects = createProjects(options);
   return {
-    commands: gallows(
-      [
-        {
-          name: 'list',
-          validate: (attributes, body) => true,
-          execute: (attributes, body) => projects.listProjects(options),
-          example: {
-            attributes: {},
-            body: ''
-          },
-          description: 'Lists all available projects.'
+    commands: gallows([
+      {
+        name: 'list',
+        validate: (attributes, body) => true,
+        execute: (attributes, body) => projects.list(),
+        example: {
+          attributes: {},
+          body: ''
         },
-        {
-          name: 'initialize',
-          validate: ({ project }) => typeof project === 'string',
-          execute: ({ project }) => projects.createProject(project, options),
-          example: {
-            attributes: { project: 'my-project' },
-            body: ''
-          },
-          description: 'Creates a new project with the specified name.'
+        description: 'Lists all available projects.'
+      },
+      {
+        name: 'initialize',
+        validate: ({ project }) => typeof project === 'string',
+        execute: ({ project }) => projects.initialize(project),
+        example: {
+          attributes: { project: 'my-project' },
+          body: ''
         },
-        // Other commands...
-      ]
-    )
+        description: 'Creates a new project with the specified name.'
+      },
+      // Other commands...
+    ])
   };
 }
