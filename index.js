4dbe61b7e7e1c944e6da761052b16fd567fce447
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-19 12:50:24 -0700

    install

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import aleister from 'aleister';
+import execution from 'phantomaton-execution';
 import Projects from './projects.js';
 
-export default aleister(Projects);
+export default configuration => {
+  const { commands, instance } = aleister(Projects)(configuration);
+  return { commands, instance, install: [
+    execution.command.provider([], () => commands)
+  ] };
+};
